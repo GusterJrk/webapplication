782a83d089d9290a20ae836f17757a9d9853b83c
GusterJrk <gustavojrknsc@gmail.com>
2022-05-27 21:04:23 +0000

    Atualiza para uma aplicação web usando Flask

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+# Ao abrir o GitPod, execute:
+# pip intall -r requirements.txt
+
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+app.run()
+
 print('Olá mundo')
